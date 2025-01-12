15e524b8cac71dd5ce6ca1ddfeae30f6649b59c0
nyaomaru <nyaonyao0725@gmail.com>
2025-01-12 20:15:51 +0900

    Study kv store

--- server/routes/test.get.dev.ts
+++ server/routes/test.get.dev.ts
@@ -1,3 +1,26 @@
-export default defineEventHandler((event) => {
-  return `this is dev environment`;
+type StorageTest = {
+  hello: string;
+};
+
+export default defineEventHandler(async (event) => {
+  await useStorage().setItem('test:foo', { hello: 'world' });
+  const storageTest1 = (await useStorage().getItem('test:foo')) as StorageTest;
+
+  await useStorage('test').setItem('foo', { hello: 'next world' });
+  const storageTest2 = (await useStorage().getItem('test:foo')) as StorageTest;
+
+  const dataStorage = useStorage('data');
+  await dataStorage.setItem('test', 'works');
+  const storageTest3 = await dataStorage.getItem('test');
+
+  await useStorage<StorageTest>('test').getItem('foo');
+  const storageTest4 = await useStorage('test').getItem<StorageTest>('foo');
+
+  return `<section>
+    <p>This is dev environment.</p>
+    <p>Storage test1: ${storageTest1.hello}</p>
+    <p>Storage test2: ${storageTest2.hello}</p>
+    <p>Storage test3: ${storageTest3}</p>
+    <p>Storage test4: ${storageTest4.hello}</p>
+    </section>`;
 });
